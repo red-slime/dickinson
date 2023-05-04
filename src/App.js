2b48de25ecb7d6a7ffe67ef35b72ef4a8d95229a
xaaane <xaneslime@gmail.com>
2023-05-04 16:48:39 -0400

    whatevber

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
 			<Header />
 			<Routes>
 				<Route path="dickinson" element={<Home />} index />
-				<Route path="../projects" element={<Projects />} />
+				<Route path="projects" element={<Projects />} />
 				<Route path="/projects/:directory" element={<ProjectViewer />} />
 				<Route path="about" element={<About />} />
 				<Route path="*" element={<NoPage />} />
