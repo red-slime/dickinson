fc22bf0e948c34b4db6bc7721068534d454c4ac9
red-slime <xaneslime@gmail.com>
2023-05-24 09:08:04 -0400

    photo updates

--- src/components/featured-list/featured.js
+++ src/components/featured-list/featured.js
@@ -1,5 +1,5 @@
 const FeaturedImages = {
-	one: require("../../assets/projects/commercial/SRP/SRP (3).jpg"),
+	one: require("../../assets/projects/commercial/SRP/SRP (1).jpg"),
 	two: require("../../assets/projects/commercial/Premier Networx/Premier-Networx (1).jpg"),
 	three: require("../../assets/projects/education/AU FAC/Augusta-University (6).jpg"),
 	four: require("../../assets/projects/healthcare/East-Central/East-Central-Regional-Hospital (5).jpg"),
